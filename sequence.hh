3279a2cc4641853702c418824e9f73351900f11b
Smitaty <57030368+Smitaty@users.noreply.github.com>
2019-10-29 15:41:06 +0100

    Update sequence.hh

--- sequence.hh
+++ sequence.hh
@@ -1,3 +1,7 @@
+/*
+Dans ce fichier est déclaré le type couleur et la classe séquence 
+*/
+
 #pragma once
 #include <array>
 
@@ -14,19 +18,19 @@ struct sequence {
     indicesequence taille;
 };
 
-void sc_initialiservide(sequence &s);
+void sc_initialiservide(sequence &s); // initialise le tableau de couleur
 
-void sc_ajouter(sequence &s, couleur c);
+void sc_ajouter(sequence &s, couleur c); // ajoute une couleur à la fin du tableau
 
 void sc_copier(sequence &s, sequence t); // copie t dans s
 
-couleur sc_acces(sequence s, int indicesequence);
+couleur sc_acces(sequence s, int ind); // renvoit la couleur à l'indice "ind" du tableau
 
-void sc_afficher(couleur c);
+void sc_afficher(couleur c); // afficher une couleur (nécessaire car couleur est un type non primitif)
 
-void sc_vider(sequence &s);
+void sc_vider(sequence &s); // vide le tableau de couleur
 
-void sc_afficher(sequence s);
+void sc_afficher(sequence s); // affiche le contenu de la séquence
 
-bool sc_comparer(sequence s, sequence t);
+bool sc_comparer(sequence s, sequence t); // compare t à s et renvoit un booléen
 
